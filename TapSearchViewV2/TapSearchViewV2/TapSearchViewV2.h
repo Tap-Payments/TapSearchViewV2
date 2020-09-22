c7bfed5d7af4132f94ea00f836c12ec61217b3bf
O Rabie <o.rabie@tap.company>
2020-09-22 14:03:13 +0200

    Upgrade syntax to match the compiler in Xcode 12+

---
+++ TapSearchViewV2/TapSearchViewV2/TapSearchViewV2.h
@@ -0,0 +1,18 @@
+//
+//  TapSearchViewV2.h
+//  TapSearchViewV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapSearchViewV2.
+FOUNDATION_EXPORT double TapSearchViewV2VersionNumber;
+
+//! Project version string for TapSearchViewV2.
+FOUNDATION_EXPORT const unsigned char TapSearchViewV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapSearchViewV2/PublicHeader.h>
+
+
